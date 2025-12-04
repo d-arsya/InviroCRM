b3b611a5c89ea7ddb904a262f7fc55ef87952fd5
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-12-04 10:06:36 +0700

    ci: Skip seeder

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -16,19 +16,85 @@ class CustomerFactory extends Factory
      */
     public function definition(): array
     {
+        $priceList = [
+            'MDW' => [
+                '1' => 7000,
+                '2' => 6000,
+            ],
+            'RO' => [
+                '1' => 10000,
+                '2' => 9000,
+            ],
+            'Aqua' => [
+                'Galon'  => 22500,
+                '220ML'  => 35000,
+                '330ML'  => 40000,
+                '600ML'  => 50000,
+                '10L'    => 19000,
+            ],
+            'Le Minerale' => [
+                'Galon'  => 21000,
+                '330ML'  => 41000,
+                '600ML'  => 51000,
+                '5L'     => 16000,
+                '1500ML'     => 55000,
+            ],
+            'Sanqua' => [
+                '120ML'  => 18000,
+                '220ML'  => 21000,
+                '330ML'  => 32000,
+                '600ML'  => 35000,
+                '1500ML' => 36000,
+            ],
+            'Pelangi' => [
+                '120ML' => 23000,
+                '220ML' => 23000,
+                '220ML B' => 26000,
+            ],
+            'Akrap' => [
+                '220ML' => 23000,
+                '600ML' => 38000,
+            ],
+            'Vit' => [
+                'Galon'   => 18000,
+                '220ML' => 23000,
+                '550ML' => 35000,
+                '1500ML' => 38000,
+            ],
+            'Gas' => [
+                '3KG'   => 23000,
+                '5,5KG'   => 110000,
+                '12KG'   => 215000,
+            ],
+        ];
+
         $productCount = fake()->numberBetween(1, 10);
 
-        $products = collect(range(1, $productCount))->map(function () {
+        // Flatten price list ke 1 list produk lengkap
+        $flatList = [];
+        foreach ($priceList as $brand => $items) {
+            foreach ($items as $unit => $price) {
+                $flatList[] = [
+                    'brand' => $brand,
+                    'unit'  => $unit,
+                    'price' => $price,
+                ];
+            }
+        }
+
+        $products = collect(range(1, $productCount))->map(function () use ($flatList) {
+            $selected = collect($flatList)->random();
+
             $quantity = fake()->numberBetween(1, 5);
-            $price = fake()->numberBetween(10, 20) * 1000;
 
             return [
-                'name' => fake()->word(),
+                'name' => $selected['brand'] . ' ' . $selected['unit'],
                 'quantity' => $quantity,
-                'price' => $price,
-                'total' => $quantity * $price,
+                'price' => $selected['price'],
+                'total' => $quantity * $selected['price'],
             ];
         })->toArray();
+
         $phones = explode(',', env('TEAM_NUMBER'));
 
         return [
